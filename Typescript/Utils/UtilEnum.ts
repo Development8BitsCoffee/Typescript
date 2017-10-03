1fa6f53f8e0385b8614183537f168142d60270e2
Eduardo Julião <eduardo.juliao@8bitscoffee.com.br>
2017-10-03 16:51:03 -0300

    Enum documentation

--- Typescript/Utils/UtilEnum.ts
+++ Typescript/Utils/UtilEnum.ts
@@ -1,16 +1,51 @@
 class UtilEnum {
+    /**
+     * Get all names and values from an enum
+     * 
+     * @static
+     * @template T 
+     * @param {*} e 
+     * @returns 
+     * @memberof UtilEnum
+     */
     static getNamesAndValues<T extends number>(e: any) {
         return UtilEnum.getNames(e).map(n => ({ name: n, value: e[n] as T }));
     }
 
+    /**
+     * Get all enum names
+     * 
+     * @static
+     * @param {*} e 
+     * @returns 
+     * @memberof UtilEnum
+     */
     static getNames(e: any) {
         return UtilEnum.getObjValues(e).filter(v => typeof v === "string") as string[];
     }
 
+    /**
+     * Get all enum values
+     * 
+     * @static
+     * @template T 
+     * @param {*} e 
+     * @returns 
+     * @memberof UtilEnum
+     */
     static getValues<T extends number>(e: any) {
         return UtilEnum.getObjValues(e).filter(v => typeof v === "number") as T[];
     }
 
+    /**
+     * Get all enum values
+     * 
+     * @private
+     * @static
+     * @param {*} e 
+     * @returns {((number | string)[])} 
+     * @memberof UtilEnum
+     */
     private static getObjValues(e: any): (number | string)[] {
         return Object.keys(e).map(k => e[k]);
     }
